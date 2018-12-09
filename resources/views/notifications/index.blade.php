cf8cdd6e5898773f943933c53dc215004c33ae02
greedisgood <hontsev@163.com>
2018-12-10 00:52:24 +0800

    补一下

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -7,6 +7,7 @@
 			<li><a href="{{ Request::url() }}?cate=all">全部通知</a></li>
 			<li><a href="{{ Request::url() }}?cate=articlereplied">文章被回复</a></li>
 			<li><a href="{{ Request::url() }}?cate=atuser">@我的消息</a></li>
+			<li><a href="{{ Request::url() }}?cate=announce">公告</a></li>
 		</ul>
 	</div>			
 			<table class="table table-hover">
